be890b0d2d87c6d60c0c8cd56a1b3f1a1c4fe805
default <default>
2024-09-25 08:58:57 +0800

    ng-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbAlert, NgbAlertModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    // NgbModule,
+    NgbAlertModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
